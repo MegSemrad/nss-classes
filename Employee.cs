a2f5ec72775c8d63a89ce031eca1cacb5cbadb7c
Meghan Semrad <mlsemrad753@gmail.com>
2021-03-04 09:50:12 -0600

    add employee and company files with their custom types

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace classes
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public string Title { get; set; }
+
+        public string StartDate { get; set; }
+    }
+}
